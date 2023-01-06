7e71b8f0a3e0f3706bd39e5fc1f9d1cf8a830f9a
khatrih <hitesh.khatri@matrixhive.com>
2023-01-06 18:59:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pokedex/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.pokedex.di
+
+import com.example.pokedex.data.remote.PokeApi
+import com.example.pokedex.repository.PokemonRepository
+import com.example.pokedex.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePokemonRepository(api: PokeApi) = PokemonRepository(api)
+
+    @Provides
+    @Singleton
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
+
+}
